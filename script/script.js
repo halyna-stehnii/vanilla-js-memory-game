4d479a7de049f960e3d2ea50e81bec6cf3adc0a8
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-01-08 23:06:15 +0100

    feat: add createCards function

--- script/script.js
+++ script/script.js
@@ -5,3 +5,33 @@ const cardBack = '🐾';
 const shuffledAnimals = [...animals, ...animals].sort(
   () => Math.random() - 0.5
 );
+
+const bodyElement = document.body;
+
+function createCards() {
+  const gameContainer = document.createElement('div');
+  gameContainer.classList.add('game-container');
+
+  shuffledAnimals.forEach((animal) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    // Create the animal emoji (front)
+    const emoji = document.createElement('div');
+    emoji.textContent = animal;
+    emoji.style.fontSize = '3rem';
+    emoji.style.display = 'none';
+
+    // Create the card back
+    const back = document.createElement('div');
+    back.textContent = cardBack;
+    back.style.fontSize = '3rem';
+
+    card.appendChild(emoji);
+    card.appendChild(back); // Add the card back
+    gameContainer.appendChild(card);
+  });
+  return gameContainer;
+}
+
+bodyElement.appendChild(createCards());
